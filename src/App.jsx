db303ccfb0810f7c4ae7284aa8d54d5092933e28
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-16 13:52:45 +0200

    feat: set DefaultLayout in Route to set a common layout to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import PostList from "./pages/PostList"
 import ChiSiamo from "./pages/ChiSiamo"
+import DefaultLayout from "./layouts/DefaultLayout"
 
 function App() {
 
@@ -9,9 +10,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/post-list' element={<PostList />} />
-          <Route path='/chi-siamo' element={<ChiSiamo />} />
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/post-list' element={<PostList />} />
+            <Route path='/chi-siamo' element={<ChiSiamo />} />
+          </Route>
         </Routes>
 
       </BrowserRouter>
