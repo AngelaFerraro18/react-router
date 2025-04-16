a0168ef6cb17352a6a94d635e60f7114494ae14f
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-16 14:16:14 +0200

    feat: install axios and create post list from api

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function PostList() {
+
+    const urlApi = 'https://jsonplaceholder.typicode.com/posts';
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        console.log('Ho caricato la pagina')
+
+        axios.get(urlApi)
+            .then(res => setPostList(res.data))
+            .catch(err => console.log(err))
+
+    }, [])
+
     return (<>
         <h2>Lista dei posts</h2>
         <ul>
-            <li>Post1</li>
-            <li>Post2</li>
-            <li>Post3</li>
+            {postList.map(post => <li key={post.id}><h3>{post.title}</h3><p>{post.body}</p></li>)}
         </ul>
     </>)
 }
